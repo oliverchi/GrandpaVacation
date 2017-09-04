2ea381fbd6297a504def621c32919fffd11e389f
oliverchi <oliver.chi@outlook.com.au>
2017-09-05 01:13:08 +1000

    init
    2

--- Resources/app.js
+++ Resources/app.js
@@ -1,20 +1,68 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
 //
-// create base UI tab and root window
+// create root window
 //
-var win1 = Titanium.UI.createWindow({
-    title:'Grandpa's Vacation',
-    backgroundColor:'#fff'
+var win = Titanium.UI.createWindow({
+    title:'Grandpa Vacation',
+    backgroundColor:'white',
+    navBarHidden: false,
+    fullscreen: true,
+    layout: 'vertical'
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+//add menu
+win.activity.onCreateOptionsMenu = function(e) {
+			var m1 = e.menu.add({
+        title : 'Close Window',
+        icon: 'images/1.png',
+        showAsAction: Ti.Android.SHOW_AS_ACTION_IF_ROOM
+      });
+			m1.addEventListener('click', function(e) {
+				win.close();
+			});
+		};
 
-win1.add(label1);
+		win.activity.onPrepareOptionsMenu = function(e) {
+			var mi = e.menu.findItem(2);
+			if (mi == null) {
+				mi = e.menu.add({
+					itemId : 2,
+					order : 1,
+					title : 'Toast',
+          icon: 'images/2.png',
+          showAsAction: Ti.Android.SHOW_AS_ACTION_IF_ROOM
+				});
+				mi.addEventListener('click', function(e) {
+					Ti.UI.createNotification({ message : "To you and yours." }).show();
+				});
+			}
+		};
+
+// HEADER
+var header = Ti.UI.createView({height:50,borderWidth:1,borderColor:'#999',backgroundColor:'white'});
+var headerLabel = Ti.UI.createLabel({color:'#777', textAlign:'center', height:'auto', text:'Header'});
+
+header.add(headerLabel);
+
+win.add(header);
+
+// BODY
+var body = Ti.UI.createView({height:Ti.UI.SIZE, layout:'vertical', backgroundColor:'#fff'});
+
+var bodyView1 = Ti.UI.createView({backgroundColor:'#336699', height:100, left:10, right:10});//For Notes
+var bodyView2 = Ti.UI.createView({backgroundColor:'#ff0000', height:50, left:10, right:10, top:10});//For Photos
+var bodyView3 = Ti.UI.createView({backgroundColor:'orange', height:50, left:10, right:10, top:10});//For Videos
+body.add(bodyView1);
+body.add(bodyView2);
+body.add(bodyView3);
+
+win.add(body);
+
+// FOOTER
+var footer = Ti.UI.createView({height:50,borderWidth:1,borderColor:'#999',backgroundColor:'white'});
+var footerLabel = Ti.UI.createLabel({color:'#777', textAlign:'center', height:'auto', text:'Footer'});//Instead by footer Links
+footer.add(footerLabel);
+
+win.add(footer);
+
+//start window
+win.open({ animated : true});
